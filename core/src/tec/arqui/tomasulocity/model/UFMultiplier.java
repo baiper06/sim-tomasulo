0caccd64ce7f00a9f6da102ebc814c357d24ad57
Emmanuel Mora Marín <emmanuel@tupera.org>
2014-05-01 16:25:10 -0600

    Fix GIt problemas

--- core/src/tec/arqui/tomasulocity/model/UFMultiplier.java
+++ core/src/tec/arqui/tomasulocity/model/UFMultiplier.java
@@ -2,4 +2,54 @@ package tec.arqui.tomasulocity.model;
 
 public class UFMultiplier extends UnitFunctional {
 
+	private final static int TIME_EXEC = 2;
+	public static int SIZE_RS = 3;
+
+	/*
+	 * Singleton
+	 */
+	private static UFMultiplier instance = null;
+	   
+	protected UFMultiplier(){
+		super(SIZE_RS, TIME_EXEC);
+		this.mReservStation = ItemReservStation[SIZE_RS];
+		this.mTimeExec = 2;
+		this.mReady = true;
+	}
+	
+	public static UFMultiplier getInstance() {
+      if(instance == null) {
+         instance = new UFMultiplier();
+      }
+      return instance;
+	}
+
+	
+	public boolean areYouReady(){
+		return this.mReady;
+	}
+	
+	public ItemReservStation action(ItemReservStation instruction){
+		int i = 0;
+		this.mReady = false;
+		
+		switch(instruction.mOperation){
+			case 1:	for(int t = 0; t = mTimeExec; t++){
+						instruction.mValue2 = instruction.mValue1 * 
+											  instruction.mValue2;
+					}
+					
+			case 2:	for(int t = 0; t = mTimeExec; t++){
+						instruction.mValue2 = instruction.mValue1 / 
+											  instruction.mValue2;
+					}
+					
+			case 3:	for(int t = 0; t = mTimeExec; t++){
+						instruction.mValue2 = instruction.mValue1 * 2;
+					}
+		}
+		
+		this.mReady = true;;
+		return instruction;
+	}
 }
